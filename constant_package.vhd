c5e5bed77b29982e91f521eab956b95d8cda4ad7
alanmasu <alan.masutti@gmail.com>
2023-04-30 18:24:18 +0200

    Aggiunta l'opcode delle le operazioni ALU non immediate

--- constant_package.vhd
+++ constant_package.vhd
@@ -10,4 +10,5 @@ package constant_package is
     constant opcode_load : std_logic_vector(6 downto 0) :=   "0000011"; 
     constant opcode_store : std_logic_vector(6 downto 0) :=  "0100011"; 
     constant opcode_alu_imm_op : std_logic_vector(6 downto 0) := "0010011";
+    constant opcode_alu_op : std_logic_vector(6 downto 0) := "0110011";
 end package ;
