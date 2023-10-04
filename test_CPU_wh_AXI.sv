15bcf49d2e8a0835b0743ea5d55be78376c89dab
Alan Masutti <alan.masutti@gmail.com>
2023-10-04 19:27:30 +0200

    Modificato il TB per la gerarchia del progetto [NOT WORK]
     - Probabilmente sbaglio qualcosa sulla gestione del master nella simulazione

--- test_CPU_wh_AXI.sv
+++ test_CPU_wh_AXI.sv
@@ -1,27 +1,33 @@
-
 `timescale 1ns / 1ps
-`include "new_rw_memory_v1_0_tb_include.svh"
+
+//Configuration current bd names
+//`define BD_NAME test_design
+`define BD_INST_NAME test_design_i
+`define BD_WRAPPER test_design_wrapper
 
 import axi_vip_pkg::*;
-import new_rw_memory_v1_0_bfm_1_slave_0_0_pkg::*;
-import new_rw_memory_v1_0_bfm_1_master_0_0_pkg::*;
+import test_design_axi_vip_0_0_pkg::*;
 
-module new_rw_memory_v1_0_tb();
+module test_CPU_wh_AXI();
+//slave vip agent
+
+//   axi_transaction                         rd_transaction;   
+//   axi_monitor_transaction                 slv_monitor_transaction;  
+//   axi_monitor_transaction                 slave_moniter_transaction_queue[$];  
+//   xil_axi_uint                            slave_moniter_transaction_queue_size =0;  
+//   xil_axi_uint                            slv_agent_verbosity = 0;  
+//   test_axi_vip_0_0_slv_mem_t              slv_agent_0;
+//   bit error_0;
+//   bit done_0;
+//   bit init_0;
+//   bit [31:0] S00_AXI_test_data[3:0]; 
+
   axi_transaction                         wr_transaction;   
-  axi_transaction                         rd_transaction;   
-  axi_monitor_transaction                 slv_monitor_transaction;  
-  axi_monitor_transaction                 slave_moniter_transaction_queue[$];  
-  xil_axi_uint                            slave_moniter_transaction_queue_size =0;  
   xil_axi_uint                            mst_agent_verbosity = 0;  
-  xil_axi_uint                            slv_agent_verbosity = 0;  
   bit                                     clock;
   bit                                     reset;
-  new_rw_memory_v1_0_bfm_1_slave_0_0_slv_mem_t          slv_agent_0;
-  bit error_0;
-  bit done_0;
-  bit init_0;
-  bit [31:0] S00_AXI_test_data[3:0]; 
    
+//Master vip agent
   xil_axi_uint                            mtestID;  
   xil_axi_ulong                           mtestADDR;  
   xil_axi_len_t                           mtestBurstLength;  
@@ -36,26 +42,33 @@ module new_rw_memory_v1_0_tb();
   xil_axi_resp_t[255:0]                   mtestRresp;  
   bit [63:0]                              mtestWDataL; 
   bit [63:0]                              mtestRDataL; 
-  new_rw_memory_v1_0_bfm_1_master_0_0_mst_t          mst_agent_0;
+  test_design_axi_vip_0_0_mst_t           mst_agent_0;
 
   `BD_WRAPPER DUT(
-    .ARESETN(reset),
-    .ACLK(clock) 
+    .reset_rtl(reset),
+    .sys_clock(clock) 
   ); 
     
   initial begin
-    slv_agent_0 = new("slave vip agent",DUT.`BD_INST_NAME.slave_0.inst.IF);
-    slv_agent_0.vif_proxy.set_dummy_drive_type(XIL_AXI_VIF_DRIVE_NONE);
-    slv_agent_0.set_agent_tag("Slave VIP");
-    slv_agent_0.set_verbosity(slv_agent_verbosity);
-    slv_agent_0.start_slave();
-    mst_agent_0 = new("master vip agent",DUT.`BD_INST_NAME.master_0.inst.IF);//ms  
+    //Slave vip agent initialization
+
+    // slv_agent_0 = new("slave vip agent",DUT.`BD_INST_NAME.slave_0.inst.IF);
+    // slv_agent_0.vif_proxy.set_dummy_drive_type(XIL_AXI_VIF_DRIVE_NONE);
+    // slv_agent_0.set_agent_tag("Slave VIP");
+    // slv_agent_0.set_verbosity(slv_agent_verbosity);
+    // slv_agent_0.start_slave();
+    
+    //Master vip agent initialization
+
+    mst_agent_0 = new("master vip agent",DUT.`BD_INST_NAME.axi_vip_0.inst.IF);//ms  
     mst_agent_0.vif_proxy.set_dummy_drive_type(XIL_AXI_VIF_DRIVE_NONE); 
     mst_agent_0.set_agent_tag("Master VIP"); 
     mst_agent_0.set_verbosity(mst_agent_verbosity); 
     mst_agent_0.start_master(); 
     $timeformat (-12, 1, " ps", 1);
   end
+
+
   initial begin
     reset <= 1'b0;
     #200ns;
@@ -71,14 +84,14 @@ module new_rw_memory_v1_0_tb();
     $finish;
   end
 
-  initial begin
-    #1;
-    forever begin
-      slv_agent_0.monitor.item_collected_port.get(slv_monitor_transaction);
-      slave_moniter_transaction_queue.push_back(slv_monitor_transaction);
-      slave_moniter_transaction_queue_size++;
-    end
-  end
+//   initial begin
+//     #1;
+//     forever begin
+//       slv_agent_0.monitor.item_collected_port.get(slv_monitor_transaction);
+//       slave_moniter_transaction_queue.push_back(slv_monitor_transaction);
+//       slave_moniter_transaction_queue_size++;
+//     end
+//   end
 
   //Modificare questo task qui per il testbench
   task automatic S_AXI_TEST;  
@@ -99,7 +112,7 @@ module new_rw_memory_v1_0_tb();
       $display("Imposto l'indirizzo nel slv_reg_2");
       mtestADDR = 32'd08; 
       mtestWDataL[31:0] = 32'h1028CC;   
-      mst_agent_0.AXI4LITE_WRITE_BURST( 
+      mst_agent_0.AXI4_WRITE_BURST( 
         mtestADDR, 
         mtestProtectionType, 
         mtestWDataL, 
@@ -110,7 +123,7 @@ module new_rw_memory_v1_0_tb();
       $display("Imposto i dati nel slv_reg_3");
       mtestADDR = 32'd12; 
       mtestWDataL[31:0] = 32'd17;    
-      mst_agent_0.AXI4LITE_WRITE_BURST( 
+      mst_agent_0.AXI4_WRITE_BURST( 
         mtestADDR, 
         mtestProtectionType, 
         mtestWDataL, 
@@ -120,7 +133,7 @@ module new_rw_memory_v1_0_tb();
       $display("start writing at 0x1028CC");
       mtestADDR = 32'd00; 
       mtestWDataL[31:0] = 32'd01;   
-      mst_agent_0.AXI4LITE_WRITE_BURST( 
+      mst_agent_0.AXI4_WRITE_BURST( 
         mtestADDR, 
         mtestProtectionType, 
         mtestWDataL, 
@@ -130,7 +143,7 @@ module new_rw_memory_v1_0_tb();
       $display("Reading BRESP");
       do begin
         mtestADDR = 32'd20; 
-        mst_agent_0.AXI4LITE_READ_BURST( 
+        mst_agent_0.AXI4_READ_BURST( 
           mtestADDR, 
           mtestProtectionType, 
           mtestRDataL, 
@@ -144,7 +157,7 @@ module new_rw_memory_v1_0_tb();
       $display("start reading");
       mtestADDR = 32'd00; 
       mtestWDataL[31:0] = 32'd02;    
-      mst_agent_0.AXI4LITE_WRITE_BURST( 
+      mst_agent_0.AXI4_WRITE_BURST( 
         mtestADDR, 
         mtestProtectionType, 
         mtestWDataL, 
@@ -153,7 +166,7 @@ module new_rw_memory_v1_0_tb();
       $display("Reading RRESP");
       do begin
         mtestADDR = 32'd16;    
-        mst_agent_0.AXI4LITE_READ_BURST( 
+        mst_agent_0.AXI4_READ_BURST( 
           mtestADDR, 
           mtestProtectionType, 
           mtestRDataL, 
@@ -164,7 +177,7 @@ module new_rw_memory_v1_0_tb();
 
       $display("Reading Data");
       mtestADDR = 32'd04;  
-      mst_agent_0.AXI4LITE_READ_BURST( 
+      mst_agent_0.AXI4_READ_BURST( 
         mtestADDR, 
         mtestProtectionType, 
         mtestRDataL, 
