ce9d39cf1f5d8bf06d0e5cac323e5148b4e50729
alanmasu <alan.masutti@gmail.com>
2025-01-09 17:59:29 +0100

    Modifiche manuali al blink

--- assembly/test_GPIO.s
+++ assembly/test_GPIO.s
@@ -1,27 +1,13 @@
 main:
-        lui     a5,0x40020
-        lw      a4,0x007(a5)
-        lbu     a5,0(a4)
-        ori     a5,a5,1
-        sb      a5,0(a4)
-        lui     a3,0x40020
-L4:
-        lw      a4,0x00B(a3)
-        lbu     a5,0(a4)
-        andi    a5,a5,-2
-        sb      a5,0(a4)
-        li      a5,999424
-        addi    a5,a5,576
+        lui     t0,0x40020
+        lw      a4,0x007(t0) # a4 <- Mem[0x40020007] (GPIOA DIR reg)
+        ori     a4,a4,1
+        sb      a4,0x007(t0) # Mem[0x40020007] <- a4
 L2:
-        addi    a5,a5,-1
-        bne     a5,zero,L2
-        lw      a4,0x00B(a3)
-        lbu     a5,0(a4)
-        ori     a5,a5,1
-        sb      a5,0(a4)
-        li      a5,999424
-        addi    a5,a5,576
-L3:
-        addi    a5,a5,-1
-        bne     a5,zero,L3
-        j       L4
+        lw      a4,0x00B(t0) # a4 <- Mem[0x4002000B] (GPIOA DATA reg)
+        andi    a4,a4,-2     # a4 <- a4 & !GPIO1    
+        sb      a4,0x00B(t0)
+        lw      a4,0x00B(t0) # a4 <- Mem[0x4002000B] (GPIOA DATA reg)
+        ori     a4,a4,1      # a4 <- a4 | GPIO1    
+        sb      a4,0x00B(t0)
+        j       L2
