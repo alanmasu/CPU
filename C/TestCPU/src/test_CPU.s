54baa3fd190ed63656b428199dbafd2ea7170c71
alanmasu <alan.masutti@gmail.com>
2025-01-16 15:06:18 +0100

    Modificato 'test_CPU.s' [NOTE ME]
     - Ho fatto delle modifiche per fare in modo che il tool di debug possa effettivamente usare lo stack per salvare i registri in memoria, sostituento x2(sp) con x20, potrebbe rompere i regression test!

--- C/TestCPU/src/test_CPU.s
+++ C/TestCPU/src/test_CPU.s
@@ -3,17 +3,18 @@
 .attribute arch, "rv32i2p1"
 .attribute unaligned_access, 0
 .attribute stack_align, 16
-.text
+# .text.main
+.section .text.main
 .align	2
 .globl	main
 .type	main, @function
 main:
     addi   x1, x0,-16       # x1 <- -16
-    addi   x2, x1, 1        # x2 <- x1 + 1  // x2 = -15
-    add    x3, x2, x1       # x3 <- x2 + x1 // x3 = -31
-    lui    x3, 0x40012      # x3 <- 0x40012 // x3 = RAM_END
-    sw     x1, -4(x3)       # Mem[x3 - 4] <- -16 // Mem[0x40011ffc] = -16   //PENSARE SE c'è un modo per testare questa istruzione
-    lw     x4, -4(x3)       # x4 <- Mem[x3 - 4] // x4 = -16
+    addi   x20, x1, 1        # x20 <- x1 + 1  // x20 = -15
+    add    x3, x20, x1      # x3 <- x20 + x1 // x3 = -31
+    lui    x3, 0x40010      # x3 <- 0x40010000 // x3 = RAM_START
+    sw     x1, 0(x3)        # Mem[x3] <- -16 // Mem[0x40010000] = -16   //PENSARE SE c'è un modo per testare questa istruzione
+    lw     x4, 0(x3)        # x4 <- Mem[x3] // x4 = -16
     beq    x4, x1, L1       # if x4 == x1, goto L1 // instr=0xff9ff06f
 L2:
     lui    x6, 0x40001      # x6 <- 0x40001
@@ -33,3 +34,6 @@ L3:
     lui    x12, 0x40000     # x12 <- 0x40000 // x12 = 0x40000000
     sw     x12, 0(x12)      # Mem[x12] <- x12 // Mem[0x40000000] = 0x40000000        // AXI WRITE
     lw     x13, 0(x12)      # x13 <- Mem[x12] // x13 = 0x40000000                    // AXI READ
+    call   cat_registers    # call cat_registers
+L4:
+    j      L4               # trap CPU
