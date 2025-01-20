f16461be6d1e4f72eca3346e2e899f96c771d8c9
alanmasu <alan.masutti@gmail.com>
2025-01-20 20:05:31 +0100

    Aggiunte modifiche per avere un registro di controllo ed azzerare il tado del TB

--- testbench/test_I2C_module.vhd
+++ testbench/test_I2C_module.vhd
@@ -59,6 +59,7 @@ architecture Behavioral of test_I2C_module is
     signal i2c_data_tb        : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
     signal i2c_data_length_tb : STD_LOGIC_VECTOR(2 downto 0) := (others => '0');
     signal i2c_rw_n_tb        : STD_LOGIC := '0';
+    signal i2c_data_to_send   : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
 
     --Slave
     type i2c_slave_state_t is (idle, start, address, data, ack1, ack2, stop);
@@ -279,9 +280,11 @@ begin
         if res = '0' then
             i2c_slave_state <= idle;
             i2c_data_count := 0;
+            i2c_data_to_send <= (others => '0');
         elsif falling_edge(i2c_sda) and i2c_slave_state = idle then
             i2c_slave_state <= start;
         elsif falling_edge(i2c_scl) and i2c_slave_state = start then
+            -- i2c_data_tb <= (others => '0');
             i2c_slave_state <= address;
         elsif rising_edge(i2c_scl) then
             if i2c_slave_state = address then
@@ -300,8 +303,9 @@ begin
                     i2c_data_tb <= (i2c_data_tb sll 1);
                     i2c_data_tb(0) <= to_X01(i2c_sda);
                 elsif i2c_rw_n_tb = '1' then            -- Incoming READ
-                    i2c_sda <= i2c_data_tb(7);
-                    i2c_data_tb <= i2c_data_tb sll 1;
+                    i2c_sda <= data_to_send(32);
+                    -- i2c_data_tb <= i2c_data_tb sll 1;
+                    data_to_send := data_to_send sll 1;
                 end if;
                 i2c_data_count := i2c_data_count + 1;
                 if(i2c_data_count = 8) then
@@ -319,17 +323,23 @@ begin
             i2c_slave_state <= i2c_next_state;
             -- Prepare for next byte if needed
             if i2c_rw_n_tb = '1' then
-                i2c_sda <= i2c_data_tb(7);
+                i2c_sda <= data_to_send(31);
                 -- i2c_data_tb <= i2c_data_tb sll 1;
             end if;
         elsif i2c_slave_state = stop and to_X01(i2c_scl) = '1' and rising_edge(i2c_sda) then
+            if i2c_rw_n_tb = '0' then
+                data_to_send(31 downto 32-i2c_data_count) := i2c_data_tb(i2c_data_count-1 downto 0);
+            end if;
             i2c_slave_state <= idle;
             i2c_data_count := 0;
+            i2c_data_to_send <= data_to_send;
         elsif i2c_slave_state = stop and falling_edge(i2c_scl) then
             if i2c_rw_n_tb = '0' then -- Incoming WRITE
                 i2c_data_tb(0) <= temp_data;
+                data_to_send := data_to_send sll 1;
+            else
+                i2c_data_tb <= (i2c_data_tb sll 1);
             end if;
-            i2c_data_tb <= (i2c_data_tb sll 1);
             i2c_data_count := 1;
             i2c_slave_state <= data;            
         end if;
