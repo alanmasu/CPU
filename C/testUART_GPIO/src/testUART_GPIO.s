64e6135211142249933c432cc3f5e5e215e1898f
alanmasu <alan.masutti@gmail.com>
2025-01-23 15:23:36 +0100

    Test addi

--- C/testUART_GPIO/src/testUART_GPIO.s
+++ C/testUART_GPIO/src/testUART_GPIO.s
@@ -13,28 +13,24 @@ main:
     lui     t1, 0x40020
     # load 'g' to x29, 'o' to x30, 'a' to x31 
     li      t4, 10           # \n  
-    li      t5, 0x67         # 'g'
-    li      t6, 0x6F         # 'o'
-    li      s7, 0x61         # 'a'
     # li      s8, 0b011111110
     li      s8, -2
 while:
     lw      a2, 0(t1)
-    mv      a0, t5           # 'g' 
+    li      a0, 'g'          # 'g' 
     jal     a1, compose
 
-    mv      a0, t6           # 'o'
+    li      a0, 'o'          # 'o'
     ori     a2, a2, 1
     jal     a1, compose
 
-    mv      a0, s7           # 'a'
-    mv      a7, a2
-    and     a2, a7, s8
+    li      a0, 'a'          # 'a'
+    and     a2, a2, s8
     jal     a1, compose
 
-    li      a6, 0b10000001
-    andi    a6, a6, -2
-    mv      a0, s7           # 'a'
+    lw      a6, 0(t1)
+    andi    a6, a6, 0b11110
+    li      a0, 'a'          # 'a'
     jal     a1, send_d
     li      a0, 'i'          # 'i'
     mv      a2, a6
