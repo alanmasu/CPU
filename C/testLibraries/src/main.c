054a4f5ffe25d6b216648a6c281dbff1659dccd8
alanmasu <alan.masutti@gmail.com>
2025-03-30 19:44:52 +0200

    Creato GPIO + printf

--- C/testLibraries/src/main.c
+++ C/testLibraries/src/main.c
@@ -7,9 +7,17 @@
 
 int main(int argc, char const *argv[]){
 
-    printf("Hello World!\n%d\n", 1234);
+    printf("Hello World!\n%d\n", 1234);\
+    gpioSetDir((uint8_t*)GPIO0_PORT_A_DIR_ADDR, PIN0 | PIN1 | PIN2 | PIN3, OUTPUT);
 
-    while(1);
+    gpioSetDir((uint8_t*)GPIO0_PORT_A_DIR_ADDR, PIN4 | PIN5 | PIN6 | PIN7, INPUT);
+
+    while(1){
+        uint8_t val = *(uint8_t*)GPIO0_PORT_A_ADDR;
+        printf("GPIOs: %02x\n", val);
+        wait(0);
+        gpioToggle((uint8_t*)GPIO0_PORT_A_DATA_ADDR, PIN0 | PIN1 | PIN2 | PIN3);
+    }
 
     return 0;
 }
